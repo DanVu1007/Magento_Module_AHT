c4faf8228f3ed13e1cdceb6e1b065cd132febbdd
DanVa <vuanhdan123@gmail.com>
2022-06-22 18:25:53 +0700

    just remain button add to cart

--- AHT/ShopByBrand/Block/Frontend/Brand/Showbrand.php
+++ AHT/ShopByBrand/Block/Frontend/Brand/Showbrand.php
@@ -30,6 +30,11 @@ class Showbrand extends \Magento\Framework\View\Element\Template
     private $productFactory;
 
     /**
+     * @var \Magento\Catalog\Helper\Image
+     */
+    private $image;
+
+    /**
      * @param \Magento\Framework\View\Element\Template\Context $context
      * @param array $data
      */
@@ -40,12 +45,14 @@ class Showbrand extends \Magento\Framework\View\Element\Template
         ScopeConfigInterface  $scopeConfig,
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productcollectionFactory,
         \Magento\Catalog\Model\ProductFactory $productFactory,
+        \Magento\Catalog\Helper\Image $image,
         array $data = []
     ) {
         $this->brandFactory = $brandFactory;
         $this->scopeConfig = $scopeConfig;
         $this->productcollectionFactory = $productcollectionFactory;
         $this->productFactory = $productFactory;
+        $this->image = $image;
         parent::__construct($context, $data);
     }
 
@@ -53,49 +60,77 @@ class Showbrand extends \Magento\Framework\View\Element\Template
     {
         $id = $this->getRequest()->getParam('id');
         $brand = $this->brandFactory->create();
-        if($id != null){
+        if ($id != null) {
             $brand = $brand->load($id);
-        }else{
+        } else {
             // redirect
         }
         return $brand;
     }
 
 
-    public function showBrandByProductID($product_id)
+    public function showBrandByProductID()
     {
-        $brand = 1;
-        //Get product by $product_id;
-
-        //Get attribute: brand_id
-
-        //$brand -> getbrand by ID
+        $result = [];
+        $id = $this->getRequest()->getParam('id');
 
         //Filter get all;
         $enable = $this->scopeConfig->getValue(self::BRAND_ENABLE, ScopeInterface::SCOPE_STORE);
-        $customBrand = $this->scopeConfig->getValue(self::CUSTOM_BRAND, ScopeInterface::SCOPE_STORE);
-        //Filter get only name
 
-        //Filter get only image
-        return $brand;
+        //Get attribute: brand_id
+        $brand_id = $this->productFactory->create()->load($id)->getBrand();
+
+        $brand = $this->brandFactory->create()->load($brand_id);
+        if ($enable == 1) {
+            //Get more filter
+            $customBrand = $this->scopeConfig->getValue(self::CUSTOM_BRAND, ScopeInterface::SCOPE_STORE);
+
+            //Filter get all
+            if ($customBrand == self::SHOW_ALL) {
+                array_push($result,['display'=>'showall']);
+            }
+            //Filter get only name
+            if ($customBrand == self::SHOW_ONLY_NAME) {
+                array_push($result,['display'=>'showname']);
+                $brand->unsetData('image');
+            }
+            //Filter get only image
+            if ($customBrand == self::SHOW_ONLY_IMAGE) {
+                array_push($result,['display'=>'showimage']);
+                $brand->unsetData('name');
+            }
+            array_push($result,$brand->getData());
+        } else {
+            $result = null;
+        }
+        return $result;
     }
 
-    public function getProductsByBrandId(){
+    //SHOW LIST PRODUCT ON DETAIL BRAND
+    public function getAllIdsByBrandId()
+    {
         $id = $this->getRequest()->getParam('id');
         $product = $this->productcollectionFactory->create();
-        if($id != null){
+        if ($id != null) {
             $product = $product->addAttributeToFilter('brand', $id)->load()->getAllIds();
         }
-
-        foreach ($product as $value) {
-            $products = $this->productFactory->create()->getById($value);
-            echo '<pre>';
-            print_r($products);
-            echo '</pre>';
-            // exit();
-        }
-
         return $product;
     }
 
+    public function getAllProduct()
+    {
+        $ids = $this->getAllIdsByBrandId();
+        $product = $this->productFactory->create();
+        $result = [];
+        foreach ($ids as $key => $id) {
+            $item = [];
+            $name = $product->load($id)->getData('name');
+            $image = $this->image->init($product->load($id), 'product_thumbnail_image')->getUrl();;
+            $price = $product->load($id)->getData('price');
+            $url = $product->load($id)->getData('url_key');
+            array_push($item, $name, $image, $price, $url);
+            array_push($result, $item);
+        }
+        return $result;
+    }
 }
