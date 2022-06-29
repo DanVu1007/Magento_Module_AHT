b6032773e1268cd411fd944f6d58e4c11a045cb2
DanVa <vuanhdan123@gmail.com>
2022-06-29 17:49:04 +0700

    save add brand name

---
+++ AHT/ShopByBrand/Plugin/BrandNameInMiniCart/ShowBrandInMiniCart.php
@@ -0,0 +1,36 @@
+<?php
+namespace AHT\ShopByBrand\Plugin\BrandNameInMiniCart;
+use Magento\Checkout\CustomerData\ItemPool;
+class ShowBrandInMiniCart
+{
+    /**
+     * @var \AHT\ShopByBrand\Model\BrandFactory
+     */
+    private $brandFactory;
+
+    /**
+     * @var \Magento\Catalog\Model\ProductFactory
+     */
+    private $productFactory;
+
+    public function __construct(
+        \AHT\ShopByBrand\Model\BrandFactory $brandFactory,
+        \Magento\Catalog\Model\ProductFactory $productFactory
+    )
+    {
+        $this->brandFactory = $brandFactory;
+        $this->productFactory = $productFactory;
+
+    }
+    public function afterGetItemData(\Magento\Checkout\CustomerData\ItemPool $itemPool, $result)
+    {
+        $productId = $result['product_id'];
+        $product = $this->productFactory->create()->load($productId);
+        $brandId = $product->getBrand();
+        $brand = $this->brandFactory->create()->load($brandId);
+        return array_push($result,['brand'=>$brand->getName()]);
+        
+    }
+
+
+}
---
+++ AHT/ShopByBrand/view/frontend/requirejs-config.js
@@ -0,0 +1,8 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Checkout/template/minicart/item/default.html': 
+                'AHT_ShopByBrand/template/minicart/item/default.html'
+          }
+    }
+};
